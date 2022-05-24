6581bfa2472fb5e48a3e21908c368f7bd5a5a0e6
Akarshgu <akarshgupta705415@gmail.com>
2022-05-24 16:43:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Student from './Component/Student.js'
+import Contact from './Component/Contact.js'
+import {BrowserRouter,Routes,Route,Link} from "react-router-dom"
+import Home from './Component/Home.js';
+import './Component/Style.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    
+      <div class="navbar navbar-expand-lg bg-dark" id='navBar'>
+          <div style={{color:"white" ,marginRight:"30vw", fontSize:"150%"}}> Welcome  </div>
+          <div><Link to="/"><button className='btn btn-outline-light'>Home </button></Link></div>
+          <div><Link to={'/Student'}><button className='btn btn-outline-light'>Student </button></Link></div>
+          <div><Link to={'Contact'}><button className='btn btn-outline-light'>Contact  </button></Link></div>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Student' element={<Student/>}/>
+        <Route path='/Contact' element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
